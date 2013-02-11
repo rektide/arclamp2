f4c292f4aab05e67b8fc5cdfd4bc5d0a85daa677
rektide <rektide@voodoowarez.com>
2013-02-11 01:19:02 -0500

    hasValue test was not running properly.

--- src/ClarinetStream.js
+++ src/ClarinetStream.js
@@ -16,7 +16,7 @@ function ClarinetStream(){
 
 	for(var i in clarinet.EVENTS){
 		var ev= clarinet.EVENTS[i],
-		  hasValue= valuedEvents.indexOf(ev)
+		  hasValue= valuedEvents.indexOf(ev) != -1
 		this.parser["on"+ev]= hasValue? function(i,a){this.outFn([i,a])}.bind(this,i): function(i){this.outFn([i])}.bind(this,i)
 	}
 	return this
