2e43c0f1c2dfbf64c92ffb4874c7227351a1c566
rektide <rektide@voodoowarez.com>
2013-05-16 05:00:00 -0400

    Update ClarinetStream for more recent Transform._transform.

--- src/ClarinetStream.js
+++ src/ClarinetStream.js
@@ -9,25 +9,23 @@ function ClarinetStream(){
 	if(!(this instanceof ClarinetStream)){
 		return new ClarinetStream()
 	}
-	stream.Transform.call(this)
+	stream.Transform.call(this,{objectMode:true})
 
 	this.parser= clarinet.parser()
-	this.outFn= null
 
 	for(var i in clarinet.EVENTS){
 		var ev= clarinet.EVENTS[i],
 		  hasValue= valuedEvents.indexOf(ev) != -1
-		this.parser["on"+ev]= hasValue? function(i,a){this.outFn([i,a])}.bind(this,i): function(i){this.outFn([i])}.bind(this,i)
+		this.parser["on"+ev]= hasValue? function(i,a){this.push([i,a])}.bind(this,i): function(i){this.push([i])}.bind(this,i)
 	}
 	return this
 }
 util.inherits(ClarinetStream, stream.Transform)
 ClarinetStream.prototype._transform= _transform
 
-function _transform(chunk, outFn, callback){
+function _transform(chunk, encoding, callback){
 	if(chunk instanceof buffer)
 		chunk= chunk.toString()
-	this.outFn= outFn
 	this.parser.write(chunk)
 	callback()
 }
